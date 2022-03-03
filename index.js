fb1bc2a105cb7ea5da83e8b2f3ef09d61b8ef1df
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-03-02 22:30:26 -0500

    commit tree 8ac63c4a42b5c0b4385a240b33f3b4a523aad0d4

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message;
+    })
+}
+
+// const userData = {
+//     userName: "Byron",
+//     userEmail: "Poodle",
+// };
+
+// const submitData = {
+//     method: "Post",
+//     headers: {
+//         "Content_Type": "application/json",
+//         Accept: "application/json"
+//     },
+//     body: JSON.stringify(userData),
+// };
+
+// fetch("http://localhost:3000/users", submitData)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     // .catch(function (error) {
+//     //     alert("Bad things! Ragnarők!");
+//     //     console.log(error.message);
+//     });
+
+
+//-----------------------------------
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "Post",
+//     headers: {
+//         "Content_Type": "application/json",
+//         Accept: "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     // .catch(function (error) {
+//     //     alert("Bad things! Ragnarők!");
+//     //     console.log(error.message);
+//     })
